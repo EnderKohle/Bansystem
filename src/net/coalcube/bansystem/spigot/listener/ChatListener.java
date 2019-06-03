5c78b6f8eaee938f309a6dd7ebe8cf3dcd50bf3a
EnderKohle <therzig2005@gmail.com>
2019-06-03 18:11:51 +0200

    Update

---
+++ src/net/coalcube/bansystem/spigot/listener/ChatListener.java
@@ -0,0 +1,46 @@
+package net.coalcube.bansystem.spigot.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import net.coalcube.bansystem.core.util.Type;
+import net.coalcube.bansystem.spigot.BanSystem;
+import net.coalcube.bansystem.spigot.util.Banmanager;
+
+public class ChatListener implements Listener {
+	
+	private Banmanager bm = new Banmanager();
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent e) {
+		if(BanSystem.mysql.isConnected()) {
+			Player p = e.getPlayer();
+			String msg = e.getMessage();
+			if(msg.startsWith("/msg") || !msg.startsWith("/")) {
+				if(bm.isbanned(p.getUniqueId()) && bm.getType(p.getUniqueId(), bm.getReasonChat(p.getUniqueId())) == Type.CHAT) {
+					if(bm.getEnd(p.getUniqueId(), bm.getReasonChat(p.getUniqueId())) > System.currentTimeMillis() || bm.getEnd(p.getUniqueId(), bm.getReasonChat(p.getUniqueId())) == -1) {
+						e.setCancelled(true);
+						for (String message : BanSystem.messages.getStringList("Ban.Chat.Screen")) {
+							p.sendMessage(message.replaceAll("%P%", BanSystem.PREFIX).replaceAll("%reason%", bm.getReasonChat(p.getUniqueId()))
+									.replaceAll("%reamingtime%", bm.getRemainingTime(p.getUniqueId(), bm.getReasonChat(p.getUniqueId()))).replaceAll("&", "§"));
+						}
+					} else {
+						bm.unmute(p.getUniqueId());
+						Bukkit.getConsoleSender().sendMessage(BanSystem.messages.getString("Ban.Chat.autounmute").replaceAll("%P%", BanSystem.PREFIX).replaceAll("%player%", p.getDisplayName()).replaceAll("&", "§"));
+						for(Player all : Bukkit.getOnlinePlayers()) {
+							if(all.hasPermission("system.ban")) {
+								all.sendMessage(BanSystem.messages.getString("Ban.Chat.autounmute")
+										.replaceAll("%P%", BanSystem.PREFIX)
+										.replaceAll("%player%", p.getDisplayName())
+										.replaceAll("&", "§"));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
